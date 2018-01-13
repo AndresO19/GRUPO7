61cfe2549397e08c3d31d0030f18d719b3bcb735
mishellviteri <>
2018-01-13 13:34:50 -0500

    subiendo Etiquetas

---
+++ GRUPO7/src/grupo7/rnegocio/entidades/Etiquetas.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package grupo7.rnegocio.entidades;
+
+import java.util.*;
+public class Etiquetas {
+    private int id_e;
+    private String nombre;
+    private Date creado;
+    private Date actualizado; 
+
+    public Etiquetas() {
+    }
+
+    public Etiquetas(int id_e, String nombre, Date creado, Date actualizado) {
+        this.id_e = id_e;
+        this.nombre = nombre;
+        this.creado = creado;
+        this.actualizado = actualizado;
+    }
+
+    public int getId_e() {
+        return id_e;
+    }
+
+    public void setId_e(int id_e) {
+        this.id_e = id_e;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Date getCreado() {
+        return creado;
+    }
+
+    public void setCreado(Date creado) {
+        this.creado = creado;
+    }
+
+    public Date getActualizado() {
+        return actualizado;
+    }
+
+    public void setActualizado(Date actualizado) {
+        this.actualizado = actualizado;
+    } 
+}

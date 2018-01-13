9749a7a252c6292dceeccd8522c32b09cdf548fc
mishellviteri <>
2018-01-13 13:50:44 -0500

    implementacion EtiquetasPublicaciones

---
+++ GRUPO7/src/grupo7/rnegocio/impl/EtiquetaspImpl.java
@@ -0,0 +1,15 @@
+package grupo7.rnegocio.impl;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author SONY
+ */
+public class EtiquetaspImpl {
+    
+}

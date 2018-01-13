05298bb3bbedb3ebc6f00308dc28dd09857538e8
Jonathan Macas <Brayan Macas@192.168.1.119>
2018-01-13 13:59:02 -0500

    Actualizacion

---
+++ GRUPO7/src/grupo7/rnegocio/dao/INiveles.java
@@ -0,0 +1,14 @@
+
+package grupo7.rnegocio.dao;
+
+import java.util.*;
+import grupo7.rnegocio.entidades.*;
+
+
+public interface INiveles {
+    public int insertar (Niveles niveles) throws Exception;
+    public int modificar (Niveles niveles) throws Exception;
+    public int eliminar (Niveles niveles) throws Exception;
+    public Niveles obtener(String codigo) throws Exception;
+    public ArrayList<Niveles> obtener () throws Exception;
+}

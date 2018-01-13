517c8bb33f816f0e7a9c709f0e5a521eea4c26bb
Jonathan Macas <Brayan Macas@192.168.1.119>
2018-01-13 14:00:43 -0500

    Actualizacion

---
+++ GRUPO7/src/grupo7/rnegocio/dao/IUsuarios.java
@@ -0,0 +1,14 @@
+
+package grupo7.rnegocio.dao;
+
+import java.util.*;
+import grupo7.rnegocio.entidades.*;
+
+
+public interface IUsuarios {
+    public int insertar (Usuarios usuarios) throws Exception;
+    public int modificar (Usuarios usuarios) throws Exception;
+    public int eliminar (Usuarios usuarios) throws Exception;
+    public Usuarios obtener(String codigo) throws Exception;
+    public ArrayList<Usuarios> obtener () throws Exception;
+}

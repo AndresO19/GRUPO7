1a2d64caa12e9eda851a221fe03dfc5ed77ab21b
Jonathan Macas <Brayan Macas@192.168.1.119>
2018-01-13 13:53:20 -0500

    Actualizacion

---
+++ GRUPO7/src/grupo7/rnegocio/dao/IEtiquetas.java
@@ -0,0 +1,14 @@
+
+package grupo7.rnegocio.dao;
+
+import java.util.*;
+import grupo7.rnegocio.entidades.*;
+
+
+public interface IEtiquetas {
+   public int insertar (Etiquetas etiquetas) throws Exception;
+    public int modificar (Etiquetas etiquetas) throws Exception;
+    public int eliminar (Etiquetas etiquetas) throws Exception;
+    public Etiquetas obtener(String codigo) throws Exception;
+    public ArrayList<Etiquetas> obtener () throws Exception; 
+}

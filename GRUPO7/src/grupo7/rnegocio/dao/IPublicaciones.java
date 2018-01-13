8576de28052a73def17fdad7e41df44f304e7141
Jonathan Macas <Brayan Macas@192.168.1.119>
2018-01-13 13:59:35 -0500

    Actualizacion

---
+++ GRUPO7/src/grupo7/rnegocio/dao/IPublicaciones.java
@@ -0,0 +1,14 @@
+
+package grupo7.rnegocio.dao;
+
+import java.util.*;
+import grupo7.rnegocio.entidades.*;
+
+
+public interface IPublicaciones {
+   public int insertar (Publicaciones publicaciones) throws Exception;
+    public int modificar (Publicaciones publicaciones) throws Exception;
+    public int eliminar (Publicaciones publicaciones) throws Exception;
+    public Publicaciones obtener(String codigo) throws Exception;
+    public ArrayList<Publicaciones> obtener () throws Exception; 
+}

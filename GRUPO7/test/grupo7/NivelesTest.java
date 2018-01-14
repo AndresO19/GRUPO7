f190aa1fc734ee8f51b9adbb6aa86a739a848e36
Mishellviteri <SONY@192.168.1.104>
2018-01-13 23:00:31 -0500

    actualizando del test  de niveles

---
+++ GRUPO7/test/grupo7/NivelesTest.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package grupo7;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import grupo7.accesodatos.*;
+import grupo7.rnegocio.dao.*;
+import grupo7.rnegocio.entidades.*;
+import grupo7.rnegocio.impl.*;
+import java.util.*;
+import java.sql.*;
+
+public class NivelesTest {
+    
+    public NivelesTest() {
+    }
+        @Test
+    public void testGeneral() {
+        INiveles nivelesDao = new NivelesImpl();
+            // TEST INSERTAR
+
+//            int filas = 0;
+//            Niveles nivel=new Niveles(2, "Bryan", new java.util.Date(), new java.util.Date());
+//            try {
+//                filas = nivelesDao.insertar(nivel);
+//                System.out.println("Ingreso de " + filas + " Filas Correctas");
+//            } catch (Exception e) {
+//                System.out.println("Error: " + e.getMessage());
+//            }
+//            assertEquals(filas > 0, true);
+//
+//            //TEST OBTENER POR CODIGO
+//        
+//        Niveles niveles=new Niveles();
+//        try {
+//            nivel=nivelesDao.obtener("1");
+//            System.out.println("            "+nivel.getId_n()+"    "+nivel.getNombre()+"    "+nivel.getCreado()+"    "+nivel.getActualizado());
+//        } catch (Exception e) {
+//        }
+//       *------------------------------------------------------------------------------------------------------------------------------------*
+        
+////            //TEST LISTADO
+            ArrayList<Niveles> niveless = new ArrayList<>();
+            try {
+                niveless = nivelesDao.obtener();
+                for (Niveles nivelt : niveless) {
+                    System.out.println(nivelt.getId_n() + " " + nivelt.getNombre() + " " + nivelt.getCreado() + " " + nivelt.getActualizado());
+                }
+            } catch (Exception e) {
+            }
+            assertEquals(niveless!= null, true);
+    }
+}
+//}
+
+

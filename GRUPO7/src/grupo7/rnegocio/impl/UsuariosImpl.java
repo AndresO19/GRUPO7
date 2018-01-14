7f858b8a59bd68a25e71a922219bff3f789b52ee
ANDRES <DELL@DESKTOP-PFTNPU5.mshome.net>
2018-01-14 03:46:48 -0500

    ACTUALIZACION usuarios impl

--- GRUPO7/src/grupo7/rnegocio/impl/UsuariosImpl.java
+++ GRUPO7/src/grupo7/rnegocio/impl/UsuariosImpl.java
@@ -1,137 +1,161 @@
-//
-//package grupo7.rnegocio.impl;
-//
-//
-//import grupo7.rnegocio.dao.*;
-//import grupo7.rnegocio.entidades.*;
-//import java.util.*;
-//import java.sql.*;
-//
-//public class UsuariosImpl {
-//    @Override
-//    public int insertar(Usuarios usuarios) throws Exception {
-//        int numFilas = 0;
-//        String sqlC = "INSERT INTO usuarios (id_u, nombre,email,pasword,id_r,creado, actualizado) VALUES (?,?,?,?,?,?,?,?)";
-//        ArrayList<Parametro> lisParametros = new ArrayList<>();
-//        lisParametros.add(new Parametro(1, usuarios.getCodigoD()));
-//        lisParametros.add(new Parametro(2, usuarios.getNombre()));
-//        lisParametros.add(new Parametro(3, usuarios.getApellido()));
-//        lisParametros.add(new Parametro(4, usuarios.gettEspecialidad()));
-//        lisParametros.add(new Parametro(5, usuarios.getHorarioC()));
-//        Conexion con = null;
-//        try {
-//            con=new Conexion();
-//            con.conectar();
-//            numFilas=con.ejecutarComando(sqlC, lisParametros);
-//        } catch (Exception e) {
-//            System.out.println("error: "+e.getMessage());
-//        } finally {
-//            if(con!=null){
-//                con.desconectar();
-//            }
-//        }
-//        return numFilas;
-//    }
-//
-//    @Override
-//    public int modificar(Usuario usuario) throws Exception {
-//        int numFilas = 0;
-////        String sqlC="UPDATE Doctor SET codigoD=?, nombre=?, apellido=? tEspecialidad=? horarioC=? WHERE codigo=?";
-////        ArrayList<Parametro> lisParametros=new ArrayList<>();
-////        lisParametros.add(new Parametro(1, doctor.getCodigoD()));
-////        lisParametros.add(new Parametro(2, doctor.getNombre()));
-////        lisParametros.add(new Parametro(3, doctor.getApellido()));
-////        lisParametros.add(new Parametro(4, doctor.gettEspecialidad()));
-////        lisParametros.add(new Parametro(5, doctor.getHorarioC()));
-////        lisParametros.add(new Parametro(6, doctor.getCodigoD()));
-////        Conexion con=null;
-////        try {
-////            con=new Conexion();
-////            con.conectar();
-////            numFilas=con.ejecutarComando(sqlC, lisParametros);
-////        } catch (Exception e) {
-////            System.out.println("Error: "+e.getMessage());
-////        } finally {
-////            if (con != null) {
-////                con.desconectar();
-////            }
-////        }
-//        return numFilas;
-//    }
-//
-//    @Override
-//    public int eliminar(Usuario usuario) throws Exception {
-//        int numFilas = 0;
-////        String sqlC="DELETE FROM Doctor WHERE codigo=?";
-////        ArrayList<Parametro> lisParametros=new ArrayList<>();
-////        lisParametros.add(new Parametro(1, doctor.getCodigoD()));
-////        Conexion con=null;
-////        try {
-////            con=new Conexion();
-////            con.conectar();
-////            numFilas=con.ejecutarComando(sqlC, lisParametros);
-////        } catch (Exception e) {
-////            System.out.println("Error: "+e.getMessage());
-////        } finally {
-////            if (con != null) {
-////                con.desconectar();
-////            }
-////        }
-//        return numFilas;
-//    }
-//
-//    @Override
-//    public Usuario obtener(String codigo) throws Exception {
-//        Usuario nUsuario = null;
-////        String sqlC="SELECT codigoD, nombre, apellido, tEspecialidad, horarioC FROM Doctor";
-////        ArrayList<Parametro> lisParametros=new ArrayList<>();
-////        lisParametros.add(new Parametro(1, codigo));
-////        Conexion con=null;
-////        try {
-////            con=new Conexion();
-////            ResultSet rst=con.ejecutarQuery(sqlC, lisParametros);
-////            while (rst.next()){
-////                nDoctor.setCodigoD(rst.getString(1));
-////                nDoctor.setNombre(rst.getString(2));
-////                nDoctor.setApellido(rst.getString(3));
-////                nDoctor.settEspecialidad(rst.getString(4));
-////                nDoctor.setHorarioC(rst.getString(5));
-////            }
-////        } catch (Exception e) {
-////            System.out.println("Error: "+e.getMessage());
-////        } finally {
-////            if (con != null) {
-////                con.desconectar();
-////            }
-////        }
-//        return nUsuario;
-//    }
-//
-//    @Override
-//    public ArrayList<Usuario> obtener() throws Exception {
-//        ArrayList<Usuario> listUsuario = new ArrayList<>();
-////        String sqlC="SELECT codigoD, nombre, apellido, tEspecialidad, horarioC FROM Doctor";
-////        Conexion con=null;
-////        try {
-////            con=new Conexion();
-////            ResultSet rst=con.ejecutarQuery(sqlC, null);
-////            
-////            while (rst.next()){
-////                Doctor nDoctor=new Doctor();
-////                nDoctor.setCodigoD(rst.getString(1));
-////                nDoctor.setNombre(rst.getString(2));
-////                nDoctor.setApellido(rst.getString(3));
-////                nDoctor.settEspecialidad(rst.getString(4));
-////                nDoctor.setHorarioC(rst.getString(5));
-////                listDoctor.add(nDoctor);
-////            }
-////        } catch (Exception e) {
-////            System.out.println("Error: "+e.getMessage());
-////        } finally {
-////            if (con != null) {
-////                con.desconectar();
-////            }
-////        }
-//        return listUsuario;
-//    }
-//}
+
+package grupo7.rnegocio.impl;
+
+import grupo7.rnegocio.impl.*;
+import grupo7.rnegocio.entidades.*;
+import grupo7.rnegocio.dao.*;
+import grupo7.accesodatos.*;
+import java.util.*;
+import java.sql.*;
+
+public class UsuariosImpl implements IUsuarios {
+    @Override
+    public int insertar(Usuarios usuarios) throws Exception {
+        int numFilas = 0;
+        String sqlC = "INSERT INTO usuarios (id_u, nombre, email, pasword, id_r, creado, actualizado) VALUES (?,?,?,?,?,?,?,?)";
+        ArrayList<Parametro> lisParametros = new ArrayList<>();
+        lisParametros.add(new Parametro(1, usuarios.getId_u()));
+        lisParametros.add(new Parametro(2, usuarios.getNombre()));
+        lisParametros.add(new Parametro(3, usuarios.getEmail()));
+        lisParametros.add(new Parametro(4, usuarios.getPasword()));
+        lisParametros.add(new Parametro(5, usuarios.getRoles()));
+        if (usuarios.getCreado() instanceof java.util.Date) {
+            lisParametros.add(new Parametro(6, new java.sql.Date(((java.sql.Date) usuarios.getCreado()).getTime())));
+        } else {
+            lisParametros.add(new Parametro(6, usuarios.getActualizado()));
+        }
+        if (usuarios.getCreado() instanceof java.util.Date) {
+            lisParametros.add(new Parametro(7, new java.sql.Date(((java.sql.Date) usuarios.getActualizado()).getTime())));
+        } else {
+            lisParametros.add(new Parametro(7, usuarios.getActualizado()));
+        }
+        
+        
+        Conexion con = null;
+        try {
+            con=new Conexion();
+            con.conectar();
+            numFilas=con.ejecutarComando(sqlC, lisParametros);
+        } catch (Exception e) {
+            System.out.println("error: "+e.getMessage());
+        } finally {
+            if(con!=null){
+                con.desconectar();
+            }
+        }
+        return numFilas;
+    }
+
+    @Override
+    public int modificar(Usuarios usuarios) throws Exception {
+        int numFilas = 0;
+        String sqlC="UPDATE usuarios SET id_u=?, nombre=?, email=?, pasword=?, id_r=?, creado=?, actualizado=? WHERE id_u=?";
+        ArrayList<Parametro> lisParametros=new ArrayList<>();
+        lisParametros.add(new Parametro(1, usuarios.getId_u()));
+        lisParametros.add(new Parametro(2, usuarios.getNombre()));
+        lisParametros.add(new Parametro(3, usuarios.getEmail()));
+        lisParametros.add(new Parametro(4, usuarios.getPasword()));
+        lisParametros.add(new Parametro(5, usuarios.getRoles()));
+        lisParametros.add(new Parametro(6, usuarios.getCreado()));
+        lisParametros.add(new Parametro(7, usuarios.getActualizado()));
+        Conexion con=null;
+        try {
+            con=new Conexion();
+            con.conectar();
+            numFilas=con.ejecutarComando(sqlC, lisParametros);
+        } catch (Exception e) {
+            System.out.println("Error: "+e.getMessage());
+        } finally {
+            if (con != null) {
+                con.desconectar();
+            }
+        }
+        return numFilas;
+    }
+
+    @Override
+    public int eliminar(Usuarios usuarios) throws Exception {
+        int numFilas = 0;
+        String sqlC="DELETE FROM usuarios WHERE id_u=?";
+        ArrayList<Parametro> lisParametros=new ArrayList<>();
+        lisParametros.add(new Parametro(1, usuarios.getId_u()));
+        Conexion con=null;
+        try {
+            con=new Conexion();
+            con.conectar();
+            numFilas=con.ejecutarComando(sqlC, lisParametros);
+        } catch (Exception e) {
+            System.out.println("Error: "+e.getMessage());
+        } finally {
+            if (con != null) {
+                con.desconectar();
+            }
+        }
+        return numFilas;
+    }
+
+    @Override
+    public Usuarios obtener(int codigo) throws Exception {
+        Usuarios nUsuario = null;
+        Roles rol=null;
+        IRoles roldao = new RolesImpl();
+        String sqlC="SELECT id_u, nombre, email, pasword, id_r, creado, actualizado FROM id_ur";
+        ArrayList<Parametro> lisParametros=new ArrayList<>();
+        lisParametros.add(new Parametro(1, codigo));
+        Conexion con=null;
+        try {
+            con=new Conexion();
+            ResultSet rst=con.ejecutarQuery(sqlC, lisParametros);
+            while (rst.next()){
+                rol = new Roles();
+                rol = roldao.obtener(rst.getString(5)); 
+                nUsuario.setId_u(rst.getInt(1));
+                nUsuario.setNombre(rst.getString(2));
+                nUsuario.setEmail(rst.getString(3));
+                nUsuario.setPasword(rst.getString(4));
+                nUsuario.setCreado(rst.getDate(6));
+                nUsuario.setActualizado(rst.getDate(7));
+            }
+        } catch (Exception e) {
+            System.out.println("Error: "+e.getMessage());
+        } finally {
+            if (con != null) {
+                con.desconectar();
+            }
+        }
+        return nUsuario;
+    }
+
+    @Override
+    public ArrayList<Usuarios> obtener() throws Exception {
+        ArrayList<Usuarios> listUsuario = new ArrayList<>();
+        Roles rol=null;
+        IRoles roldao = new RolesImpl();
+        String sqlC="SELECT id_u, nombre, email, pasword, id_r, creado, actualizado FROM usuarios";
+        Conexion con=null;
+        try {
+            con=new Conexion();
+            ResultSet rst=con.ejecutarQuery(sqlC, null);
+            Usuarios user=null;
+            while (rst.next()){
+                rol = new Roles();
+                rol = roldao.obtener(rst.getString(5)); 
+                user = new Usuarios();
+                user.setId_u(rst.getInt(1));
+                user.setNombre(rst.getString(2));
+                user.setEmail(rst.getString(3));
+                user.setPasword(rst.getString(4));
+                user.setCreado(rst.getDate(6));
+                user.setActualizado(rst.getDate(7));
+                listUsuario.add(user);
+            }
+        } catch (Exception e) {
+            System.out.println("Error: "+e.getMessage());
+        } finally {
+            if (con != null) {
+                con.desconectar();
+            }
+        }
+        return listUsuario;
+    }
+}

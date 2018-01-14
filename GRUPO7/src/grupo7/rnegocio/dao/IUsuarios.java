6007f7f32b76fd833d79905ce72cdcff2292336c
ANDRES OROZCO <DELL@DESKTOP-PFTNPU5.mshome.net>
2018-01-14 01:50:32 -0500

    ACTUALIZACION USUARIOS INTERNET

--- GRUPO7/src/grupo7/rnegocio/dao/IUsuarios.java
+++ GRUPO7/src/grupo7/rnegocio/dao/IUsuarios.java
@@ -9,6 +9,6 @@ public interface IUsuarios {
     public int insertar (Usuarios usuarios) throws Exception;
     public int modificar (Usuarios usuarios) throws Exception;
     public int eliminar (Usuarios usuarios) throws Exception;
-    public Usuarios obtener(String codigo) throws Exception;
+    public Usuarios obtener(int codigo) throws Exception;
     public ArrayList<Usuarios> obtener () throws Exception;
 }

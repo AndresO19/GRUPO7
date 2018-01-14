f784542e8fa33e29d978147f1d3cfe7f074c4cf3
ANDRES <DELL@DESKTOP-PFTNPU5.mshome.net>
2018-01-14 16:39:51 -0500

    creacion test usuarios

---
+++ GRUPO7/test/grupo7/Usuariotest.java
@@ -0,0 +1,21 @@
+
+package grupo7;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author DELL
+ */
+public class Usuariotest {
+    
+    public Usuariotest() {
+    }
+    
+    
+}

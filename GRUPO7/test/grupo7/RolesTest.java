313f2e9374493bb1ddcdde85ea14d75108160be6
ANDRES OROZCO <DELL@DESKTOP-PFTNPU5.mshome.net>
2018-01-15 12:09:41 -0500

    actualizacion roles

--- GRUPO7/test/grupo7/RolesTest.java
+++ GRUPO7/test/grupo7/RolesTest.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package grupo7;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -17,48 +18,49 @@ import grupo7.rnegocio.impl.*;
 import java.util.*;
 import java.sql.*;
 
-
-
 public class RolesTest {
-    
+
     public RolesTest() {
     }
-     @Test
+
+    @Test
     public void testGeneral() {
         IRoles rolesDao = new RolesImpl();
-            // TEST INSERTAR
-
-            int filas = 0;
-            Roles rol=new Roles(3, "Michu", new java.util.Date(), new java.util.Date());
-            try {
-                filas = rolesDao.insertar(rol);
-                System.out.println("Ingreso de " + filas + " Filas Correctas");
-            } catch (Exception e) {
-                System.out.println("Error: " + e.getMessage());
-            }
-            assertEquals(filas > 0, true);
-//
-//          ///  TEST OBTENER POR CODIGO
+        // TEST INSERTAR
+////
+////            int filas = 0;
+////            Roles rol=new Roles(3, "Michu", new java.util.Date(), new java.util.Date());
+////            try {
+////                filas = rolesDao.insertar(rol);
+////                System.out.println("Ingreso de " + filas + " Filas Correctas");
+////            } catch (Exception e) {
+////                System.out.println("Error: " + e.getMessage());
+////            }
+////            assertEquals(filas > 0, true);
+////
+////          ///  TEST OBTENER POR CODIGO
+////        
+//            
+        Roles roless=new Roles();
         
-            
-      //  Roles roles=new Roles();
-//        
+        try {
+            roless=rolesDao.obtener(3);
+            System.out.println("            "+roless.getId_r()+"    "+roless.getNombre()+"    "+roless.getCreado()+"    "+roless.getActualizado());
+        } catch (Exception e) {
+        }
+        
+          
+//        //*------------------------------------------------------------------------------------------------------------------------------------*
+////////        
+//            //TEST LISTADO
+//        ArrayList<Roles> roless = new ArrayList<>();
 //        try {
-//            rol=rolesDao.obtener("1");
-//            System.out.println("            "+rol.getId_r()+"    "+rol.getNombre()+"    "+rol.getCreado()+"    "+rol.getActualizado());
+//            roless = rolesDao.obtener();
+//            for (Roles rolt : roless) {
+//                System.out.println(rolt.getId_r() + " " + rolt.getNombre() + " " + rolt.getCreado() + " " + rolt.getActualizado());
+//            }
 //        } catch (Exception e) {
 //        }
-////        //*------------------------------------------------------------------------------------------------------------------------------------*
-//////        
-////            //TEST LISTADO
-            ArrayList<Roles> roless = new ArrayList<>();
-            try {
-                roless = rolesDao.obtener();
-                for (Roles rolt : roless) {
-                    System.out.println(rolt.getId_r() + " " + rolt.getNombre() + " " + rolt.getCreado() + " " + rolt.getActualizado());
-                }
-            } catch (Exception e) {
-            }
-            assertEquals(roless!= null, true);
+//        assertEquals(roless != null, true);
     }
 }

9db394bffa2bf471ee64ed6cbee10782e212d330
ANDRES OROZCO <DELL@DESKTOP-PFTNPU5.mshome.net>
2018-01-15 13:42:12 -0500

    actualizacion publicacionestest

--- GRUPO7/test/grupo7/Publicacionestest.java
+++ GRUPO7/test/grupo7/Publicacionestest.java
@@ -39,33 +39,29 @@ public class Publicacionestest {
 //        assertEquals(filas > 0, true);
 
         ////LISTAR POR CODIGO TEST
-       
-        Publicaciones up=new Publicaciones();
-        
-        try {
-            up=puDao.obtener(1);
-            System.out.println("CODIGO_P " + " CODIGO_U" + " CODIGO_N" + " TITULO " + " CONTENIDO " + " VISTAS " + " VOTOS " + " CREADO " + "      ACTUALIZADO " );
-            System.out.println(up.getId_p()+"             "+    up.getUsuarios().getId_u() +"        "+up.getNiveles().getId_n()+"     "+up.getTitulo()+"      "+up.getContenido()+"     "+up.getVistas()+"      "+up.getVotos()+"    "+up.getCreado()+"   "+up.getActualizado());
-        } catch (Exception e) { 
-            System.out.println("error: "+e.getMessage());
-        }
-       assertEquals(up != null, true);
-////////        //// test listado
-//       
-//        ArrayList<Publicaciones> ups = new ArrayList<>();
+//        Publicaciones up = new Publicaciones();
+//
 //        try {
-//            ups = puDao.obtener();
-//            for (Publicaciones nup : ups) {
-//                System.out.println(nup.getId_p() + "" + nup.getUsuarios().getId_u() + "" + nup.getNiveles().getId_n() + " " + nup.getTitulo() + " " + nup.getContenido() + " " + nup.getVistas() + " " + nup.getVotos() + " " + nup.getCreado() + " " + nup.getActualizado());
-//            }
+//            up = puDao.obtener(1);
+//            System.out.println("CODIGO_P " + " CODIGO_U" + " CODIGO_N" + " TITULO " + " CONTENIDO " + " VISTAS " + " VOTOS " + " CREADO " + "      ACTUALIZADO ");
+//            System.out.println(up.getId_p() + "             " + up.getUsuarios().getId_u() + "        " + up.getNiveles().getId_n() + "     " + up.getTitulo() + "      " + up.getContenido() + "     " + up.getVistas() + "      " + up.getVotos() + "    " + up.getCreado() + "   " + up.getActualizado());
 //        } catch (Exception e) {
 //            System.out.println("error: " + e.getMessage());
 //        }
-//        assertEquals(ups != null, true);
-
-
-
-
+//        assertEquals(up != null, true);
+//////////        //// test listado
+//       
+        ArrayList<Publicaciones> ups = new ArrayList<>();
+        try {
+            ups = puDao.obtener();
+            for (Publicaciones nup : ups) {
+                System.out.println("CODIGO_P " + " CODIGO_U" + " CODIGO_N" + " TITULO " + " CONTENIDO " + " VISTAS " + " VOTOS " + " CREADO " + "      ACTUALIZADO ");
+                System.out.println(nup.getId_p() + "             " + nup.getUsuarios().getId_u() + "        " + nup.getNiveles().getId_n() + "     " + nup.getTitulo() + "      " + nup.getContenido() + "     " + nup.getVistas() + "      " + nup.getVotos() + "    " + nup.getCreado() + "   " + nup.getActualizado());
+            }
+        } catch (Exception e) {
+            System.out.println("error: " + e.getMessage());
+        }
+        assertEquals(ups != null, true);
 
     }
 }

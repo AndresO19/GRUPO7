9666fff6d258de3ca26cf6dc9e2b658efd5f480a
ANDRES OROZCO <DELL@DESKTOP-PFTNPU5.mshome.net>
2018-01-15 12:35:23 -0500

    creacion test publicaciones

---
+++ GRUPO7/test/grupo7/Publicacionestest.java
@@ -0,0 +1,17 @@
+
+package grupo7;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+class Publicacionestest {
+    
+    public Publicacionestest() {
+    }
+    
+    
+}

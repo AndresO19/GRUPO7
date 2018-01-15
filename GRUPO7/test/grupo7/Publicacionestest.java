e0294ad9a759a293ad036378c1c962fa5dc298cb
ANDRES <DELL@DESKTOP-PFTNPU5.mshome.net>
2018-01-15 13:27:34 -0500

    actualizacion

--- GRUPO7/test/grupo7/Publicacionestest.java
+++ GRUPO7/test/grupo7/Publicacionestest.java
@@ -21,45 +21,51 @@ public class Publicacionestest {
     @Test
     public void testGeneral() {
         IPublicaciones puDao = new PublicacionesImpl();
-
-        int filas = 0;
-
-        Niveles nivels = new Niveles(1, "Bryan", new java.util.Date(), new java.util.Date());
-        Roles rols = new Roles(1, "Michu", new java.util.Date(), new java.util.Date());
-        Usuarios user = new Usuarios(3, "Daniel", "ao@gmail.com", "1295", rols, new java.util.Date(), new java.util.Date());
-        Publicaciones per = new Publicaciones(1, user, nivels, "casa", "ropa", 1, 1, 1.5, new java.util.Date(), new java.util.Date());
-        
-
-        try {
-            filas = puDao.insertar(per);
-            System.out.println("Filas insertadas: " + filas);
-        } catch (Exception e) {
-            System.out.println("Error: " + e.getMessage());
-        }
-        assertEquals(filas > 0, true);
-
-        ////test codigo
-//        
-//        Usuarios user=new Usuarios();
-//        try {
-//            user=usDao.obtener(1);
-//            System.out.println(user.getId_u()+" "+user.getNombre()+" "+user.getEmail()+" "+user.getPasword()+" "+user.getRoles().getId_r()+" "+user.getCreado()+" "+user.getActualizado());
-//        } catch (Exception e) {
-//            System.out.println("error: "+e.getMessage());
-//        }
-//       assertEquals(user != null, true);
+        ///////////INSERTAR TEST
+//        int filas = 0;
 //
-////////        //// test listado
-////       
-//        ArrayList<Usuarios> usuarioss = new ArrayList<>();
+//        Niveles nivels = new Niveles(1, "Bryan", new java.util.Date(), new java.util.Date());
+//        Roles rols = new Roles(1, "Michu", new java.util.Date(), new java.util.Date());
+//        Usuarios user = new Usuarios(3, "Daniel", "ao@gmail.com", "1295", rols, new java.util.Date(), new java.util.Date());
+//        Publicaciones per = new Publicaciones(1, user, nivels, "casa", "ropa", 1, 1, 1.5, new java.util.Date(), new java.util.Date());
+//        
+//
 //        try {
-//            usuarioss = usDao.obtener();
-//            for (Usuarios nusuarios : usuarioss) {
-//                System.out.println(nusuarios.getId_u() + " " + nusuarios.getNombre() + " " + nusuarios.getEmail() + " " + nusuarios.getPasword() + " " + nusuarios.getRoles().getId_r() + " " + nusuarios.getCreado() + " " + nusuarios.getActualizado());
+//            filas = puDao.insertar(per);
+//            System.out.println("Filas insertadas: " + filas);
+//        } catch (Exception e) {
+//            System.out.println("Error: " + e.getMessage());
+//        }
+//        assertEquals(filas > 0, true);
+
+        ////LISTAR POR CODIGO TEST
+       
+        Publicaciones up=new Publicaciones();
+        
+        try {
+            up=puDao.obtener(1);
+            System.out.println("CODIGO_P " + " CODIGO_U" + " CODIGO_N" + " TITULO " + " CONTENIDO " + " VISTAS " + " VOTOS " + " CREADO " + "      ACTUALIZADO " );
+            System.out.println(up.getId_p()+"             "+    up.getUsuarios().getId_u() +"        "+up.getNiveles().getId_n()+"     "+up.getTitulo()+"      "+up.getContenido()+"     "+up.getVistas()+"      "+up.getVotos()+"    "+up.getCreado()+"   "+up.getActualizado());
+        } catch (Exception e) { 
+            System.out.println("error: "+e.getMessage());
+        }
+       assertEquals(up != null, true);
+////////        //// test listado
+//       
+//        ArrayList<Publicaciones> ups = new ArrayList<>();
+//        try {
+//            ups = puDao.obtener();
+//            for (Publicaciones nup : ups) {
+//                System.out.println(nup.getId_p() + "" + nup.getUsuarios().getId_u() + "" + nup.getNiveles().getId_n() + " " + nup.getTitulo() + " " + nup.getContenido() + " " + nup.getVistas() + " " + nup.getVotos() + " " + nup.getCreado() + " " + nup.getActualizado());
 //            }
 //        } catch (Exception e) {
-//
+//            System.out.println("error: " + e.getMessage());
 //        }
-//        assertEquals(usuarioss != null, true);
+//        assertEquals(ups != null, true);
+
+
+
+
+
     }
 }
